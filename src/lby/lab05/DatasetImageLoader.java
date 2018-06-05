a3da92dd80d2d5ed72949936190e99612fb32540
Nate <intoqasa@gmail.com>
2018-06-05 14:01:10 +0800

    Now it at least can read a dir

--- src/lby/lab05/DatasetImageLoader.java
+++ src/lby/lab05/DatasetImageLoader.java
@@ -1,5 +1,19 @@
 package lby.lab05;
 
+import java.io.File;
+import java.io.IOException;
+
 public class DatasetImageLoader {
+    File folder = new File("/Users/m78");
+    File[] listOfFiles = folder.listFiles();
+
+    public void loadImages() throws IOException {
+        System.out.println(folder.getName());
+        for (File file: listOfFiles) {
+            if (file.isFile()) {
+                System.out.println(file.getName());
+            }
+        }
+    }
 
 }
