39cd8fee6eea04ac62ae13074ccf5c07700d79dc
Nate <intoqasa@gmail.com>
2018-06-06 09:58:08 +0800

    It can read from a relative path

--- src/lby/lab05/DatasetImageLoader.java
+++ src/lby/lab05/DatasetImageLoader.java
@@ -2,12 +2,17 @@ package lby.lab05;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class DatasetImageLoader {
-    File folder = new File("/Users/m78");
+
+    File folder = new File("./src/lby/lab05/digits/trainingDigits");
     File[] listOfFiles = folder.listFiles();
 
     public void loadImages() throws IOException {
+
+        ArrayList<Image> images = new ArrayList<>();
+
         System.out.println(folder.getName());
         for (File file: listOfFiles) {
             if (file.isFile()) {
