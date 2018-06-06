a5dcf65dd04e727b68b3b26d2bcfa472b3ed6dff
Nate <intoqasa@gmail.com>
2018-06-06 11:48:06 +0800

    Able to sort Distance now

--- src/lby/lab05/Distance.java
+++ src/lby/lab05/Distance.java
@@ -1,4 +1,35 @@
 package lby.lab05;
 
-public class Distance {
+public class Distance implements Comparable<Distance> {
+    // MARK: - Field
+    private String tag;
+    private double distance;
+
+    // Constructor
+    public Distance(String tag, double distance) {
+        this.tag = tag;
+        this.distance = distance;
+    }
+
+    // MARK: - Getters
+    public String getTag() {
+        return tag;
+    }
+
+    public double getDistance() {
+        return distance;
+    }
+
+    // MARK: - Implement compareTo() method
+    @Override
+    public int compareTo(Distance o) {
+        if (distance < o.getDistance()) {
+            return -1;
+        } else if (distance == o.getDistance()) {
+            return 0;
+        } else if (distance > o.getDistance()) {
+            return 1;
+        }
+        return 0;
+    }
 }
